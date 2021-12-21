8643228caa28be22fe8220e698e31223b5eb3cc9
Daniel Gresak <daniel.gresak91@gmail.com>
2021-12-21 13:10:16 +0000

    refactor into functions

--- main.py
+++ main.py
@@ -8,35 +8,42 @@ from pathlib import Path
 dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-today = datetime.today().strftime("%d/%m/%Y")
-
-# This is getting the date of 7 days ahead of us. This is to compare a week from now to assignments due
-today_week = datetime.today() + timedelta(days=+7)
-# This is to format the date correctly so it'll match the format from the spreadsheet
-today_week = today_week.strftime("%d/%m/%Y")
-
-# This is what I use to send the message to discord so it arrives at the correct place.
-webhook = Webhook.from_url(DISCORD_WEBHOOK, adapter=RequestsWebhookAdapter())
-
-# This is my sheety Auth and endpoint to retrieve information from the spreadsheet.
-SHEETY_ENDPOINT = os.getenv("SHEETY_ENDPOINT")
-
-# Sending an API request to SHEETY to get the data
-response = requests.get(url=f"{SHEETY_ENDPOINT}").json()
-sheet_details = response["sheet1"]
+def get_webhook():
+    # This is what I use to send the message to discord so it arrives at the correct place.
+    DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
+    return Webhook.from_url(DISCORD_WEBHOOK, adapter=RequestsWebhookAdapter())
 
 
-# Looping through the sheet
-for assignment in sheet_details:
-    # Checking if today's date is the same as the reminder date
-    try:
-        if assignment['date'] == today_week:
-            # Sending message to discord.
-            message = f"We have an assignment, {assignment['assignment']} for {assignment['module']} due on the {assignment['date']}. " \
-                      f"This assignment is worth {assignment['percentage'] * 100}% of the module's final grade."
-            webhook.send(message)
-    # Catch Keyerror's with spaces in sheet
-    except KeyError:
-        pass
+def get_today_week():
+    # This is getting the date of 7 days ahead of us. This is to compare a week from now to assignments due
+    today_week = datetime.today() + timedelta(days=+7)
+    # This is to format the date correctly so it'll match the format from the spreadsheet
+    return today_week.strftime("%d/%m/%Y")
+
+
+def get_sheet_deet():
+    # This is my sheety Auth and endpoint to retrieve information from the spreadsheet
+    endpoint = os.getenv("SHEETY_ENDPOINT")
+    # Sending an API request to SHEETY to get the data
+    response = requests.get(url=endpoint).json()
+    return response["sheet1"]
+
+
+def main(sheet_details, today_week):
+    # Looping through the sheet
+    for assignment in sheet_details:
+        # Checking if today's date is the same as the reminder date
+        try:
+            if assignment['date'] == today_week:
+                # Sending message to discord.
+                message = f"We have an assignment, {assignment['assignment']} for {assignment['module']} due on the {assignment['date']}. " \
+                          f"This assignment is worth {assignment['percentage'] * 100}% of the module's final grade."
+                webhook = get_webhook()
+                webhook.send(message)
+        # Catch KeyErrors with spaces in sheet
+        except KeyError:
+            pass
+
+
+main(get_sheet_deet(), get_today_week())
